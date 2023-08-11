c82ece4c54a4cfc1af2e57799a6df728b9124123
MVDW <maxvdwolf@gmail.com>
2023-08-11 15:50:27 +0200

    add textformat util

---
+++ text.py
@@ -0,0 +1,30 @@
+import re
+
+class format:
+    def __init__(self, input):
+        self.input = input;
+
+    def center(self):
+
+        strlen = self.ret_strlen();
+        size = 34 - (strlen/2);
+        center = "";
+        i = 0;
+        while i < size:
+            center += " ";
+            i = i + 1;
+
+        self.input = center + self.input;
+
+    def ret_strlen(self):
+        strip = re.compile(r"""
+            \x1b     # literal ESC
+            \[       # literal [
+            [;\d]*   # zero or more digits or semicolons
+            [A-Za-z] # a letter
+        """, re.VERBOSE).sub
+        temp = strip("", self.input)
+        return len(temp);
+
+    def ret(self):
+        return self.input
