d3314795f624347cb8f6162b2b5a0f622ea91ad5
MVDW <maxvdwolf@gmail.com>
2023-08-11 14:55:32 +0200

    add config loader

---
+++ config.py
@@ -0,0 +1,21 @@
+from .logger import log
+
+import yaml
+
+class configloader:
+    def __init__(self, type):
+        mapping = {
+            "config": ("config.yml"),
+            "edition": ("edition-configuration.yml"),
+            "sources": ("upstream-sources.yml")
+        }
+        if type in mapping:
+            self.file = open(mapping[type], 'r');
+        else:
+            log.fail("Invailid mapping for loading '" + type + "'");
+            exit();
+
+    def load(self):
+        return yaml.safe_load(self.file);
+
+       
