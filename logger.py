6760d2f9732e33033e81ceee8428ef5f81eb162a
BruddaCode <fabio.wolthuis@gmail.com>
2024-06-09 00:13:51 +0200

    fixed string formatting. removed unnecessary ;'s

--- logger.py
+++ logger.py
@@ -3,26 +3,26 @@ import sys
 class log:
 
     def info(self, end = "\n"):
-        sys.stderr.write("[INFO] " + self + end);
+        sys.stderr.write(f"[INFO] {self}{end}")
 
     def ok(self, end = "\n"):
-        sys.stderr.write("[ \x1b[0;32mOK\x1b[1;0m ] " + self + end);
+        sys.stderr.write(f"[ \x1b[0;32mOK\x1b[1;0m ] {self}{end}")
 
     def warn(self, end = "\n"):
-        sys.stderr.write("[\x1b[0;93mWARN\x1b[1;0m] " + self + end);
+        sys.stderr.write(f"[\x1b[0;93mWARN\x1b[1;0m] {self}{end}")
 
     def fail(self, end = "\n"):
-        sys.stderr.write("[\x1b[1;31mFAIL\x1b[1;0m] " + self + end);
+        sys.stderr.write(f"[\x1b[1;31mFAIL\x1b[1;0m] {self}{end}")
     
     def print(self, end = "\n"):
-        sys.stderr.write("       " + self + end);
+        sys.stderr.write(f"       {self}{end}")
 
     def tree(self, start = True, end = True, tree = 0):
 
-        i=0;
+        i=0
         
         for line in self:
-            symbol = "├╴";
+            symbol = "├╴"
 
             if i == 0 and start:
                 symbol = "╭╴"
@@ -30,17 +30,17 @@ class log:
                 symbol = "╰╴"
 
             
-            i2 = 0;
-            spacing = "";
+            i2 = 0
+            spacing = ""
             while i2 < tree:
-                spacing += "│  ";
-                i2=i2+1;
+                spacing += "│  "
+                i2=i2+1
 
             if isinstance(line, list):
                 log.tree(line, False, True, tree+1)
             else:
-                log.print(spacing + symbol + line);
+                log.print(f"{spacing}{symbol}{line}")
 
-            i=i+1;
+            i=i+1
         if tree > 0:
             log.print(spacing)
